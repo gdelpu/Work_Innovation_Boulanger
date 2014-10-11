f6b3b6c442535c55ce2acfde0a7b64e600b8223c
Grégory DELPU <gdelpu@gmail.com>
2014-10-11 09:52:56 +0200

    Project creation

---
+++ Carie_Boulanger/Carie_Boulanger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carie_Boulanger
+//
+//  Created by gregory delpu on 11/10/2014.
+//  Copyright (c) 2014 Gregory DELPU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Carie_Boulanger/Carie_BoulangerTests/Carie_BoulangerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Carie_BoulangerTests.swift
+//  Carie_BoulangerTests
+//
+//  Created by gregory delpu on 11/10/2014.
+//  Copyright (c) 2014 Gregory DELPU. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Carie_BoulangerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

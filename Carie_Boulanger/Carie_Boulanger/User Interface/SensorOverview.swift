6423027f3baf4f2335217e3c477dbd434062353c
Grégory DELPU <gdelpu@gmail.com>
2014-10-12 00:30:07 +0200

    Custom view added for sensors

---
+++ Carie_Boulanger/Carie_Boulanger/User Interface/SensorOverview.swift
@@ -0,0 +1,21 @@
+//
+//  SensorOverview.swift
+//  Carie_Boulanger
+//
+//  Created by gregory delpu on 11/10/2014.
+//  Copyright (c) 2014 Gregory DELPU. All rights reserved.
+//
+
+import UIKit
+
+class SensorOverview: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
